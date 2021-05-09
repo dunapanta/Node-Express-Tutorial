dc926f325c90bab79e5ee2a13cbf92319d9fa2c4
Daniel Unapanta <dunapanta2@gmail.com>
2021-05-09 08:19:03 -0500

    Working with req and res

--- 1-node/9-http-module/http-module.js
+++ 1-node/9-http-module/http-module.js
@@ -1,8 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("Home Page");
-  res.end();
+  //console.log(req);
+  if (req.url === "/") {
+    res.end("Home Page");
+  } else if (req.url === "/about") {
+    res.end("About Page");
+  } else {
+    res.end(`
+      <h1>Not Found</h1>
+      <p>Error al navegar</p>
+      <a href="/">Home</a>
+      `);
+  }
 });
 
 server.listen(5000);
