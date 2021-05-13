1f4c33842ffb24ab0778c94464b1569afa2a50b9
Daniel Unapanta <dunapanta2@gmail.com>
2021-05-12 19:45:30 -0500

    using Promise insted of callback for async

---
+++ 1-node/11-async-patterns/2-await-pattern.js
@@ -0,0 +1,27 @@
+const { readFile } = require("fs");
+
+/* readFile("./1-node/11-async-patterns/text.txt", "utf8", (err, data) => {
+  if (err) {
+    return;
+  } else {
+    console.log(data);
+  }
+});
+ */
+
+// Transformando en Promesa
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./1-node/11-async-patterns/text.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
